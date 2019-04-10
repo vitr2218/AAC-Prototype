ff3e4633eedc77b0019d59c0159e60c9215890a1
vitr2218 <vitr2218@colorado.edu>
2019-04-10 02:17:59 -0600

    uploaded

---
+++ aac.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+
+$(document).ready(function() { // do this when the document is loaded
+    $("#home").show(); // home page
+    $("#funfacts").hide(); // hide pages 
+
+	
+});
+
+$("#b4").click(function(event) {
+	$("#funfacts").show();
+	$("#home").hide()
+});
+
