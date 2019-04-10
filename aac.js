236bde8443c2ea2dd044b55ff6472d5268f63909
qlychee <eqiu21@gmail.com>
2019-04-10 05:28:27 -0600

    categories page and filler fact

--- aac.js
+++ aac.js
@@ -3,12 +3,21 @@
 $(document).ready(function() { // do this when the document is loaded
     $("#home").show(); // home page
     $("#funfacts").hide(); // hide pages 
+    $("#fact").hide(); // example fact
 
 	
 });
 
-$("#b4").click(function(event) {
-	$("#funfacts").show();
-	$("#home").hide()
+$("#b4").click(function (event) {
+    $("#home").hide()
+    $("#funfacts").show();
+    $("#fact").hide(); // example fact
+    
 });
 
+$("#c2").click(function (event) {
+    $("#home").hide()
+    $("#funfacts").hide();
+    $("#fact").show(); // example fact
+
+});
